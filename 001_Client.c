37780728ce2495f674d452d7a64caccbf79133c0
luiie <dbsldkfls@ewhain.net>
2025-11-29 17:24:58 +0900

    03 TCP Server/Client

--- 001_Client.c
+++ 001_Client.c
@@ -16,24 +16,28 @@ int main(int argc, char **argv)
     int strLength = 0;
     int index = 0, readLength = 0;
 
-    if(argc!=3)
+    if(argc != 3)
     {
         printf("usage : %s <IP> <port> \n", argv[0]);
         exit(1);
     }
 
+    // Create client socket
     aSocket = socket(PF_INET, SOCK_STREAM, 0);
     if(aSocket == -1)
         errorHandling("socket() error");
 
+    //Initialize server socket
     memset(&serverAddress, 0, sizeof(serverAddress));
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_addr.s_addr = inet_addr(argv[1]);
     serverAddress.sin_port = htons(atoi(argv[2]));
 
+    //Connect to the server
     if(connect(aSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1)
         errorHandling("connect() error!!");
 
+    //Receive data byte-by-byte with boundary check
     while(readLength = read(aSocket, &message[index++], 1))
     {
         if(strLength == -1)
@@ -41,10 +45,14 @@ int main(int argc, char **argv)
         
         strLength += readLength;
     }
+    message[strLength] = '\0';
 
     printf("Message from server : %s \n", message);
     printf("Function read call counter : %d \n", strLength);
+
+    //Close the socket
     close(aSocket);
+
     return 0;
 }
 
--- 001_Server.c
+++ 001_Server.c
@@ -17,7 +17,7 @@ int main(int argc,char **argv)
     int cilntAddressSize;
     char message[]="Hello HANY!";
 
-    if(argc =! 2)
+    if(argc != 2)
     {
         printf("Usage : %s <port> ",argv[0]);
        exit(1);
@@ -34,18 +34,24 @@ int main(int argc,char **argv)
     serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
     serverAddress.sin_port = htons(atoi(argv[1]));
 
-    //Assign an address to socket
+    //Assign an IP address and port to the server socket
     if(bind(serverSock, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) == -1)
         errorHandling("bind() error");
+    //Change the socket state to listening for client connections
     if(listen(serverSock, 5) == -1)
         errorHandling("listen() error");
+
+    //Accept the connection request from the first client in the waiting queue
     cilntAddressSize = sizeof(cilntAddress);
     clintSock = accept(serverSock, (struct sockaddr*)&cilntAddress,&cilntAddressSize);
-
     if(clintSock == -1)
         errorHandling("accept() error");
+
     write(clintSock, message, sizeof(message));
+
+    //Close the socket
     close(clintSock);
+    
     return 0;
 }
 
